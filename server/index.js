539b3cc2149469c08a6ce186ba25b10ad4312f70
Nicolas Mingorance-Geraldo <nmingora@uwo.ca>
2024-02-10 18:12:35 -0500

    googleConfig.js pushed -> not sure if it works. may need to remove

--- server/index.js
+++ server/index.js
@@ -4,6 +4,19 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const router = express.Router();
 
+// Import database modules
+const mysql = require('mysql');
+const dbConfig = require('./googleConfig.js');
+// Create a connection to the database
+const connection = mysql.createConnection(dbConfig);
+// Open the MySQL connection
+connection.connect(error => {
+    if (error) throw error;
+    console.log('Successfully connected to the database.');
+})
+
+
+
 //setup middleware to do logging
 app.use((req, res, next, err) => { //for all routes
     next(); //keep going
