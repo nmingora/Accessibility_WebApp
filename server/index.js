f5d1b13e2c774a08d2838f549855286aa5a6f80d
Gmore <gmore@uwo.ca>
2024-02-12 16:28:20 -0500

    signup starting code

--- server/index.js
+++ server/index.js
@@ -15,8 +15,6 @@ connection.connect(error => {
     console.log('Successfully connected to the database.');
 })
 
-
-
 //setup middleware to do logging
 app.use((req, res, next) => { //for all routes
     console.log(req.method, req.url)
@@ -38,6 +36,43 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'build', 'static')))
 
 //install router at /api/uptown
 app.use('/api/uptown', router);
+
+//users endpoint
+app.post('/api/users', (req, res) => {
+    const {userid, name, email, create_time} = req.body;
+
+    const sql = 'INSERT INTO users (userid, name, email, create_time) VALUES (?, ?, ?)';
+    connection.query(sql, [userid, name, email, create_time], (err, results) => {
+        if(err) {
+            console.error('Error adding user:', error);
+            res.status(500).json({ error: 'An error occurred while adding user.' });
+        } else {
+            res.json({ message: 'User added successfully.' });
+        }
+    })
+})
+
+app.post('/signup', (req, res) => {
+    const {email, password} = req.body;
+})
+
+
+//10. Static Signup
+router.route('/user/signup') 
+    .post(async(req, res) => {
+        const {email, password} = req.body;
+
+        try {
+            const user = await User.signup(email, password);
+
+            //create token
+            const token = createToken(user._id);
+
+            res.status(200).json({email, token});
+        } catch (error){
+            res.status(400).json({error: error.message});
+        }
+    });
 
 //get list of all users who have signed up
 router.route('/')
