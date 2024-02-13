42ce54507b905c0aaedee3b5727173e6d5b8951f
Gmore <gmore@uwo.ca>
2024-02-12 20:38:31 -0500

    took something out

--- server/index.js
+++ server/index.js
@@ -66,24 +66,6 @@ app.post('/signup', (req, res) => {
     })
 })
 
-
-//10. Static Signup
-router.route('/user/signup') 
-    .post(async(req, res) => {
-        const {email, password} = req.body;
-
-        try {
-            const user = await User.signup(email, password);
-
-            //create token
-            const token = createToken(user._id);
-
-            res.status(200).json({email, token});
-        } catch (error){
-            res.status(400).json({error: error.message});
-        }
-    });
-
 //get list of all users who have signed up
 router.route('/')
     .get((res, req) => {
