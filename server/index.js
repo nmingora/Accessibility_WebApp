d71316bd5a0bfa44961f144a2671d5c5f43ad03f
geaston2 <113630555+geaston2@users.noreply.github.com>
2024-02-16 17:28:31 -0500

    Added application Post endpoint, lmk if broke

--- server/index.js
+++ server/index.js
@@ -70,18 +70,48 @@ app.listen(PORT, () => {
 
 
 //signup endpoint
-app.post('/signup', (req, res) => {
-    const {fName, lName, birthDate, userName, password, email, phoneNumber, address, notes} = req.body;
+app.put("/apply",(req,res)=>{
+    try{
+        const {fName,lName,username,password,phoneNumber,dob,address,notes} = req.body;
+        const insertData = [fName,lName,username,password,phoneNumber,dob,address,notes,'NULL',new Date(),'Pending'];
+        validApplication = false;
 
-    const sql = 'INSERT INTO signedUpUser (fName, lName, birthDate, userName, password, email, phoneNumber, address, notes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    connection.query(sql, [fName, lName, birthDate, userName, password, email, phoneNumber, address, notes], (error) => {
-        if(error) {
-            console.error('Error signing up user: ', error);
-            res.status(500).json({error: 'An error occured while signing up user.'});
-        } else {
-            res.json({message: 'User signed up successfully.'});
-        }
-    });
+        //if phone number has been taken by existing user
+        const takenField = `SELECT SUM(count) AS total_count FROM(SELECT COUNT(*) AS count FROM User WHERE username='${username}' UNION SELECT COUNT(*) AS count FROM User WHERE email='${email}' UNION SELECT COUNT(*) AS count FROM User WHERE contact='${phoneNumber}') AS subquery;`
+
+        //if multiple submissions (same email & contact) in 1 week
+        const pendingSubmissions = `SELECT COUNT(*) AS count FROM Application WHERE email='${email}' AND contact='${phoneNumber}' AND appStatus='Pending';`
+
+        //if previously denied submissions in 1 month
+        const deniedSubmissions = `SELECT COUNT(*) AS count FROM Application WHERE email='${email}' AND contact='${phoneNumber}' AND appStatus='Denied' AND applicationDate >= DATE_SUB(CURRENT_DATE(),INTERVAL 1 MONTH);`
+
+        connection.query(takenField,function(err,takenResult){
+            if(err) throw err;
+            if(takenResult[0].total_count!==0){
+                res.sendStatus(401).json({"message":"(401) Unauthorized Application: username, email, and/or phone number already taken"});
+            }
+        });
+        connection.query(pendingSubmissions,function(err,pendingResult){
+            if(err) throw err;
+            if(pendingResult[0].count!==0){
+                res.sendStatus(401).json({"message":"(401) Unauthorized Application: Multiple pending submissions, please wait response"})
+            }
+        });
+        connection.query(deniedSubmissions,function(err,deniedResult){
+            if(err) throw err;
+            if(deniedResult[0].count!==0){
+                res.sendStatus(401).json({"message":"(401) Unauthorized Application: Previously denied, please wait 1 month before resubmission"})
+            }
+        })
+
+        connection.query("INSERT INTO Application (fName,lName,username,pass,email,contact,DOB,addr,notes,reviewingAdmin,applicationDate,appStatus) VALUES ?",insertData,function(err,result){
+            if(err) throw err;
+            res.sendStatus(202).json({"message":"Application successfully submitted! An admin will contact you"})
+        });
+    }catch(err){
+        console.log(err);
+        res.sendStatus(500).json({"message":"(500) Unexpected error occured during submission"})
+    }
 });
 
 
