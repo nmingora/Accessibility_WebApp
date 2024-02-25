1d3ba1f547b2829c2157db7ccaf5fd75c73cde89
Mlo83 <mlo83@uwo.ca>
2024-02-24 23:05:43 -0500

    working post endpoint for posts forum

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto');// for id generation bc i don't want packages
 
 // Post Model
 const Post = require('../models/Post');
 
+// Generate a unique postID with 4 alphanumeric characters
+function generatePostID() {
+    return crypto.randomBytes(2).toString('hex');
+  }
+
+
+
+
+//____________________________________________________________________//
+//                          a get general                              //
 // @route GET api/posts
 // @desc Get All Posts
 router.get('/', async (req, res) => {
@@ -15,6 +26,30 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Add routes for CREATE, UPDATE, DELETE...
+
+
+//____________________________________________________________________//
+
+//                          post a new post                              //
+// @route POST api/posts
+// @desc Create a Post
+router.post('/', async (req, res) => {
+    try {
+      const { postHeader, postParagraph } = req.body;
+      const newPost = new Post({
+        postID: generatePostID(), // Generate a unique postID
+        userEmail: "publicUser@tempdefault.com", // Default user email
+        postHeader: postHeader,
+        postParagraph: postParagraph
+      });
+  
+      const savedPost = await newPost.save();
+      res.status(201).json(savedPost);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  });
+  //____________________________________________________________________//
+
 
 module.exports = router;
