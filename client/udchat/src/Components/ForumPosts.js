1d1649e19d4568a0d8bdf7960f0b7bb6bd29815d
Mlo83 <mlo83@uwo.ca>
2024-03-11 21:08:23 -0400

    changing things to base URL

--- client/udchat/src/Components/ForumPosts.js
+++ client/udchat/src/Components/ForumPosts.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './ForumPosts.css'; // Make sure the path is correct
 import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../config';  // Importing from the src directory
+
 
 const ForumPosts = () => {
   const [posts, setPosts] = useState([]);
@@ -57,7 +59,7 @@ const ForumPosts = () => {
 
   const fetchPosts = async () => {
     try {
-      const response = await fetch(`http://localhost:5004/api/posts?page=${currentPage}&limit=${postsPerPage}`);
+      const response = await fetch(`${BASE_URL}/api/posts?page=${currentPage}&limit=${postsPerPage}`);
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
@@ -88,7 +90,7 @@ const ForumPosts = () => {
     console.log('Submitting:', payload); // Debugging
   
     try {
-      const response = await fetch('http://localhost:5004/api/posts', {
+      const response = await fetch(`${BASE_URL}/api/posts`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
