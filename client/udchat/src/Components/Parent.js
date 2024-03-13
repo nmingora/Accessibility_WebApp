e34addd392589cceb38a6658b788f054ad184cc7
Daniel Korkut <dkorkut@uwo.ca>
2024-03-13 14:12:14 -0400

    Parent CSS

--- client/udchat/src/Components/Parent.js
+++ client/udchat/src/Components/Parent.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Layout from './Layout';
 import { BASE_URL } from '../config';  // Importing from the src directory
+import "./Parent.css";
 
 const Parent = () => {
   const [username, setUsername] = useState('');
@@ -96,49 +97,50 @@ navigate('/ForumPosts');
 }
   
 
-  return (
-
-    <Layout>
-    <div>
-      <h2>Parent Login</h2>
-      <h3>Welcome Parent! Please log in to view the parent dashboard</h3>
-
-      {!isLoggedIn ? (
-        <form onSubmit={handleLogin}>
-          <div>
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-      ) : (
-        <>
-        <button onClick={handleLogout}>Logout</button> // Logout button visible only when logged in
-        <button onClick={toForumPosts}>Go to Community Forum!</button>
-         </>
-
-      )}
-      {loginMessage && <p>{loginMessage}</p>}
-      
-    </div>
-  </Layout>
-);
+    return (
+      <Layout>
+      <div className="parent-login-container">
+        <h2 className="parent-login-title">Parent Login</h2>
+        <h3 className="parent-login-description">Welcome Parent! Please Log In To View The Parent Dashboard!</h3>
+    
+        {!isLoggedIn ? (
+          <form className="login-form" onSubmit={handleLogin}>
+            <div>
+              <label className="username-css">Username:</label>
+              <input
+                className="login-input"
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label className="password-css">Password:</label>
+              <input
+                className="login-input"
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="login-button-container">
+            <button className="login-button" type="submit">Login</button>
+            </div>
+          </form>
+        ) : (
+          <>
+            <button className="logout-button" onClick={handleLogout}>Logout</button>
+            <button className="forum-button" onClick={toForumPosts}>Go to Community Forum!</button>
+          </>
+        )}
+        {loginMessage && <p className="login-message">{loginMessage}</p>}
+      </div>
+    </Layout>
+  );
 };
 
     
